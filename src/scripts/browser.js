d9c2a8c4f164519bcb0cea3fb07a2b74a8ced3ea
Alexey <alex_kremlev@vk.com>
2024-08-02 17:15:29 +0500

    minor fix

--- src/scripts/browser.js
+++ src/scripts/browser.js
@@ -1,8 +1,9 @@
 import Bowser from 'bowser'
+import { detectMobile } from './helper'
 
 const browser = Bowser.getParser(window.navigator.userAgent)
 const name = browser.getBrowserName().toLowerCase()
 
-if (name.includes('firefox') || name.includes('safari')) {
+if (name.includes('firefox') || name.includes('safari') || detectMobile()) {
     document.body.classList.add('not-blured')
 }
